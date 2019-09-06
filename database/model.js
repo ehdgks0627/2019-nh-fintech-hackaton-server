5c0ef737331b8f059e2d4544b4407d14e316b689
HubCodes <krlrhkstk12@gmail.com>
2019-09-06 14:50:37 +0900

    Add nutrient schema

--- database/model.js
+++ database/model.js
@@ -2,13 +2,13 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 exports.UserSchema = new Schema({
-	_id: Schema.Types.ObjectId,
-	name: String,
-	password: String,
-	weight: Number,
-	height: Number,
-	gender: Boolean,
-	age: Number,
+    _id: Schema.Types.ObjectId,
+    name: String,
+    password: String,
+    weight: Number,
+    height: Number,
+    gender: Boolean,
+    age: Number,
     cart_id: Number,
     order_id: [Number],
 });
@@ -38,12 +38,23 @@ exports.OrderSchema = new Schema({
     order_id: Number,
     order_status: Number,
     cart_id: Number,
-    order_timestamp: { type : Date, default: Date.now },
+    order_timestamp: { type: Date, default: Date.now },
 })
 
 exports.NutrientSchema = new Schema({
     nutrient_id: Number,
-    
-    //영양소 테이블 컬럼 추가 필요
-
+    code: String, // 식품 코드
+    category: String, // 식품군
+    name: String, // 이름
+    Eng: Number, // 열량, kcal
+    Car: Number, // 탄수화물, g
+    Pro: Number, // 단백질, g
+    Fat: Number, // 지방, g
+    Tsg: Number, // 당류, g
+    Na: Number, // 나트륨, mg
+    CHOLE: Number, // 콜레스테롤, mg
+    SFA: Number, // 포화지방산, g
+    TFA: Number, // 트랜스지방산, g
+    name_english: String, // 영문 이름
+    origin: String, // 원산지
 });
