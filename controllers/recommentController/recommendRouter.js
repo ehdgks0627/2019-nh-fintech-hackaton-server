a1ac4454849265deb44001dbbddbdede3ab1f952
HubCodes <krlrhkstk12@gmail.com>
2019-09-07 12:04:31 +0900

    change

--- controllers/recommentController/recommendRouter.js
+++ controllers/recommentController/recommendRouter.js
@@ -57,7 +57,7 @@ router.get('/', async (req, res) => {
                 },
                 {
                     status: summaryNutInfo[4],
-                    recommend: await Product.findOne({ product_category: "김" })
+                    recommend: await Product.findOne({ product_category: "후추" })
                 }
             ]
         });
